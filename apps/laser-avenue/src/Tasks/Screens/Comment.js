5f70721f355c220850b49655e695410e30ca0d93
ferasdiab <dfiras96@gmail.com>
2022-06-13 09:18:12 +0300

    edit and delete comment

--- apps/laser-avenue/src/Tasks/Screens/Comment.js
+++ apps/laser-avenue/src/Tasks/Screens/Comment.js
@@ -10,6 +10,8 @@ import {
   Alert,
   Modal,
   FlatList,
+  Pressable,
+  TouchableHighlight,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import axios from 'axios';
@@ -30,6 +32,8 @@ export default function Comment({
   const [replayPage, setReplayPage] = useState(1);
   const [replayComments, setreplayComments] = useState([]);
   const [maxReplayPage, setMaxReplayPage] = useState(1);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [editModalVisible, setEditModalVisible] = useState(false);
 
   useEffect(() => {
     if (comment) {
@@ -51,6 +55,7 @@ export default function Comment({
   /* -------------------------- delete comment ----------------------------------- */
 
   const openDialogDeleteComment = () => {
+    setModalVisible(false);
     Alert.alert('Delete this comment', `Do you want to delete this comment ?`, [
       {
         text: 'Cancel',
@@ -122,6 +127,7 @@ export default function Comment({
         });
         setComments(arr);
         setUpdateText(false);
+        setEditModalVisible(false);
       }
     } catch (error) {
       console.log('error');
@@ -176,104 +182,118 @@ export default function Comment({
     }
   };
 
+  const showModel = () => {
+    try {
+      if (userId === comment.comment.user_id) setModalVisible(!modalVisible);
+    } catch (error) {}
+  };
+
+  const editInModal = () => {
+    setModalVisible(false);
+    setEditModalVisible(true);
+  };
+
   /* ------------------------------------------------------------------------ */
 
   return (
-    <View style={style.comment_cont}>
-      <View style={style.comment}>
-        <View
-          style={{
-            width: '22%',
-          }}
-        >
-          <Image
-            style={{ width: 60, height: 60, borderRadius: 30, marginTop: 5 }}
-            source={{
-              uri: `https://randomuser.me/api/portraits/men/10.jpg`,
-            }}
-          />
-        </View>
-        <View style={{ width: '70%' }}>
-          <View style={{ flexDirection: 'row' }}>
-            <Text style={style.userName}> {comment.comment.user_name} </Text>
-            <View style={{ flexDirection: 'row' }}>
-              {userId === comment.comment.user_id ? (
-                <>
-                  <Icon
-                    name="pencil"
-                    style={{ marginRight: 15 }}
-                    size={18}
-                    color="#009688"
-                    onPress={() => {
-                      setUpdateText(!updateText);
-                    }}
-                  />
-                  <Icon
-                    name="close"
-                    size={18}
-                    style={{ color: 'red' }}
-                    onPress={openDialogDeleteComment}
-                  />
-                </>
-              ) : null}
-            </View>
-          </View>
-          {!updateText ? (
-            <Text style={style.text}> {comment.comment.comment} </Text>
-          ) : (
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <View style={{ width: '70%' }}>
-                <TextInput
-                  onChangeText={handleInput}
-                  value={str}
-                  placeholder="new comment"
-                />
-              </View>
-              <Icon
-                name="check"
-                size={20}
-                style={{ color: 'blue' }}
-                onPress={updateComment}
-              />
-            </View>
-          )}
-        </View>
-      </View>
-      {comment.replays.length ? (
-        <View style={{ alignItems: 'flex-end' }}>
-          <View style={{ width: '70%' }}>
-            <Text
-              style={{ color: 'blue', textTransform: 'capitalize' }}
-              onPress={() => {
-                setShowReplay(!showReplay);
+    <>
+      <View style={style.comment_cont}>
+        <Pressable onLongPress={showModel}>
+          <View style={style.comment}>
+            <View
+              style={{
+                width: '22%',
               }}
             >
-              {showReplay ? 'hide replays' : 'show replays'}
-            </Text>
-          </View>
-        </View>
-      ) : null}
-      {showReplay ? (
-        <View style={{ alignItems: 'center' }}>
-          <View style={{ width: '75%', margin: 10, minHeight: 280 }}>
-            <FlatList
-              data={replayComments}
-              renderItem={({ item, index }) => {
-                return (
-                  <CommentReplay
-                    replay={item}
-                    comments={comments}
-                    setComments={setComments}
-                    taskCreator={taskCreator}
-                    userId={userId}
+              <Image
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  marginTop: 5,
+                }}
+                source={{
+                  uri: `https://randomuser.me/api/portraits/men/10.jpg`,
+                }}
+              />
+            </View>
+            <View style={{ width: '70%' }}>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={style.userName}>
+                  {' '}
+                  {comment.comment.user_name}{' '}
+                </Text>
+                <View style={{ flexDirection: 'row' }}>
+                  {userId === comment.comment.user_id ? (
+                    <>
+                      <Icon
+                        name="ellipsis-v"
+                        style={{ marginRight: 15 }}
+                        size={18}
+                        color="#009688"
+                        onPress={showModel}
+                      />
+                    </>
+                  ) : null}
+                </View>
+              </View>
+              {!updateText ? (
+                <Text style={style.text}> {comment.comment.comment} </Text>
+              ) : (
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <View style={{ width: '70%' }}>
+                    <TextInput
+                      onChangeText={handleInput}
+                      value={str}
+                      placeholder="new comment"
+                    />
+                  </View>
+                  <Icon
+                    name="check"
+                    size={20}
+                    style={{ color: 'blue' }}
+                    onPress={updateComment}
                   />
-                );
-              }}
-              keyExtractor={(item) => item.replay_id}
-            />
+                </View>
+              )}
+            </View>
           </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-            {/* {replayPage !== 1 ? (
+        </Pressable>
+        {comment.replays.length ? (
+          <View style={{ alignItems: 'flex-end' }}>
+            <View style={{ width: '70%' }}>
+              <Text
+                style={{ color: 'blue', textTransform: 'capitalize' }}
+                onPress={() => {
+                  setShowReplay(!showReplay);
+                }}
+              >
+                {showReplay ? 'hide replays' : 'show replays'}
+              </Text>
+            </View>
+          </View>
+        ) : null}
+        {showReplay ? (
+          <View style={{ alignItems: 'center' }}>
+            <View style={{ width: '75%', margin: 10, minHeight: 280 }}>
+              <FlatList
+                data={replayComments}
+                renderItem={({ item, index }) => {
+                  return (
+                    <CommentReplay
+                      replay={item}
+                      comments={comments}
+                      setComments={setComments}
+                      taskCreator={taskCreator}
+                      userId={userId}
+                    />
+                  );
+                }}
+                keyExtractor={(item) => item.replay_id}
+              />
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+              {/* {replayPage !== 1 ? (
               <Text
                 style={{margin: 10}}
                 onPress={() => {
@@ -282,40 +302,173 @@ export default function Comment({
                 back
               </Text>
             ) : null} */}
-            {/* <Text style={{margin: 10}}> {replayPage} </Text> */}
-            {replayPage !== maxReplayPage ? (
-              <Text
-                style={{ margin: 10 }}
-                onPress={() => {
-                  setReplayPage(replayPage + 1);
-                }}
-              >
-                Show more
-              </Text>
-            ) : null}
+              {/* <Text style={{margin: 10}}> {replayPage} </Text> */}
+              {replayPage !== maxReplayPage ? (
+                <Text
+                  style={{ margin: 10 }}
+                  onPress={() => {
+                    setReplayPage(replayPage + 1);
+                  }}
+                >
+                  Show more
+                </Text>
+              ) : null}
+            </View>
           </View>
-        </View>
-      ) : null}
-      <View style={{ alignItems: 'center' }}>
-        <View
-          style={{
-            width: '85%',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <View>
-            <TextInput
-              onChangeText={handleReplayText}
-              value={replayText}
-              placeholder="Enter your replay here"
-            />
+        ) : null}
+        <View style={{ alignItems: 'center' }}>
+          <View
+            style={{
+              width: '85%',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View>
+              <TextInput
+                onChangeText={handleReplayText}
+                value={replayText}
+                placeholder="Enter your replay here"
+              />
+            </View>
+            <Icon name="plus" size={22} onPress={addReplay} />
           </View>
-          <Icon name="plus" size={22} onPress={addReplay} />
         </View>
       </View>
-    </View>
+      {/* ------------------------- modal ------------------------------------ */}
+      {/* ----------------------------------------------------------------------- */}
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={style.centeredView2}>
+          <View style={style.modalViewStatus}>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#009688', borderColor: '#009688' },
+                ]}
+                onPress={editInModal}
+              >
+                <Text style={style.textStyle}>Edit</Text>
+                <Icon
+                  name="pencil"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#ff5252', borderColor: '#ff5252' },
+                ]}
+                onPress={openDialogDeleteComment}
+              >
+                <Text style={style.textStyle}>Delete</Text>
+                <Icon
+                  name="trash-o"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#2A416A', borderColor: '#2A416A' },
+                ]}
+                onPress={() => {
+                  setModalVisible(false);
+                }}
+              >
+                <Text style={style.textStyle}>Close</Text>
+                <Icon
+                  name="close"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={editModalVisible}
+      >
+        <View style={style.centeredViewEdit}>
+          <View style={style.modalViewStatusEdit}>
+            <View style={{ width: '100%', marginTop: 25 }}>
+              <View style={{ width: '100%', alignItems: 'center' }}>
+                <View style={{ backgroundColor: '#F5F4F4', width: '85%' }}>
+                  <TextInput
+                    onChangeText={handleInput}
+                    value={str}
+                    placeholder="new comment"
+                    style={{ width: '100%' }}
+                  />
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                width: '100%',
+                paddingHorizontal: 15,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 25,
+              }}
+            >
+              <Pressable
+                onPress={() => {
+                  setEditModalVisible(false);
+                }}
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: '#2A416A',
+                  paddingTop: 10,
+                  paddingBottom: 10,
+                  paddingRight: 20,
+                  paddingLeft: 20,
+                  borderRadius: 10,
+                  borderWidth: 1,
+                  borderColor: '#2A416A',
+                }}
+              >
+                <Text style={{ color: 'white', fontSize: 15, marginRight: 5 }}>
+                  Close
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={updateComment}
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: '#009688',
+                  paddingTop: 10,
+                  paddingBottom: 10,
+                  paddingRight: 10,
+                  paddingLeft: 10,
+                  borderRadius: 10,
+                  borderWidth: 1,
+                  borderColor: '#009688',
+                }}
+              >
+                <Text style={{ color: 'white', fontSize: 15, marginRight: 5 }}>
+                  Update
+                </Text>
+                <Icon name="check" size={20} style={{ color: 'white' }} />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
 
@@ -343,5 +496,78 @@ const style = StyleSheet.create({
     marginTop: 10,
     color: 'black',
     fontSize: 13,
+  },
+  centeredView2: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalViewStatus: {
+    width: '100%',
+    height: 280,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    paddingTop: 20,
+    alignItems: 'flex-start',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  centeredViewEdit: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  modalViewStatusEdit: {
+    width: '100%',
+    height: 480,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    paddingTop: 20,
+    alignItems: 'flex-start',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  button_con: {
+    marginTop: 15,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '60%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    marginRight: 10,
   },
 });
--- apps/laser-avenue/src/Tasks/Screens/CommentReplay.js
+++ apps/laser-avenue/src/Tasks/Screens/CommentReplay.js
@@ -10,6 +10,7 @@ import {
   Alert,
   Modal,
   FlatList,
+  Pressable,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import axios from 'axios';
@@ -23,6 +24,8 @@ export default function CommentReplay({
 }) {
   const [updateText, setUpdateText] = useState(false);
   const [str, setStr] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [editModalVisible, setEditModalVisible] = useState(false);
 
   useEffect(() => {
     setStr(replay.comment);
@@ -33,6 +36,7 @@ export default function CommentReplay({
   };
 
   const openDialogDeleteReplay = () => {
+    setModalVisible(false);
     Alert.alert('Delete this replay', `Do you want to delete this replay ?`, [
       {
         text: 'Cancel',
@@ -102,6 +106,7 @@ export default function CommentReplay({
         },
       });
       if (res.status) {
+        setEditModalVisible(false);
         const arr = comments.map((ele) => {
           if (ele.comment.comment_id === replay.comment_id) {
             return {
@@ -126,68 +131,205 @@ export default function CommentReplay({
     } catch (error) {}
   };
 
+  const showModel = () => {
+    try {
+      if (userId === replay.user_id) setModalVisible(!modalVisible);
+    } catch (error) {}
+  };
+
+  const editInModal = () => {
+    setModalVisible(false);
+    setEditModalVisible(true);
+  };
+
   /* ----------------------------------------------------------------------------------------------- */
 
   return (
-    <View style={style.comment}>
-      <View
-        style={{
-          width: '22%',
-        }}
-      >
-        <Image
-          style={{ width: 50, height: 50, borderRadius: 25, marginTop: 5 }}
-          source={{
-            uri: `https://randomuser.me/api/portraits/men/10.jpg`,
+    <>
+      <Pressable onLongPress={showModel} style={style.comment}>
+        <View
+          style={{
+            width: '22%',
           }}
-        />
-      </View>
-      <View style={{ width: '70%' }}>
-        <View style={{ flexDirection: 'row' }}>
-          <Text style={style.userName}> {replay.user_name} </Text>
-          <View style={{ flexDirection: 'row' }}>
-            {userId === replay.user_id ? (
-              <>
-                <Icon
-                  name="pencil"
-                  style={{ marginRight: 15 }}
-                  size={18}
-                  color="#009688"
-                  onPress={() => {
-                    setUpdateText(!updateText);
-                  }}
-                />
-                <Icon
-                  name="close"
-                  size={18}
-                  style={{ color: 'red' }}
-                  onPress={openDialogDeleteReplay}
-                />
-              </>
-            ) : null}
-          </View>
+        >
+          <Image
+            style={{ width: 50, height: 50, borderRadius: 25, marginTop: 5 }}
+            source={{
+              uri: `https://randomuser.me/api/portraits/men/10.jpg`,
+            }}
+          />
         </View>
-        {!updateText ? (
-          <Text style={style.text}> {replay.comment} </Text>
-        ) : (
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <View style={{ width: '70%' }}>
-              <TextInput
-                onChangeText={handleInput}
-                value={str}
-                placeholder="new comment"
+        <View style={{ width: '70%' }}>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={style.userName}> {replay.user_name} </Text>
+            <View style={{ flexDirection: 'row' }}>
+              {userId === replay.user_id ? (
+                <>
+                  <Icon
+                    name="ellipsis-v"
+                    style={{ marginRight: 15 }}
+                    size={18}
+                    color="#009688"
+                    onPress={showModel}
+                  />
+                </>
+              ) : null}
+            </View>
+          </View>
+          {!updateText ? (
+            <Text style={style.text}> {replay.comment} </Text>
+          ) : (
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View style={{ width: '70%' }}>
+                <TextInput
+                  onChangeText={handleInput}
+                  value={str}
+                  placeholder="new comment"
+                />
+              </View>
+              <Icon
+                name="check"
+                size={20}
+                style={{ color: 'blue' }}
+                onPress={updateReplay}
               />
             </View>
-            <Icon
-              name="check"
-              size={20}
-              style={{ color: 'blue' }}
-              onPress={updateReplay}
-            />
+          )}
+        </View>
+      </Pressable>
+      {/* ------------------------- modal ------------------------------------ */}
+      {/* ----------------------------------------------------------------------- */}
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={style.centeredView2}>
+          <View style={style.modalViewStatus}>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#009688', borderColor: '#009688' },
+                ]}
+                onPress={editInModal}
+              >
+                <Text style={style.textStyle}>Edit</Text>
+                <Icon
+                  name="pencil"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#ff5252', borderColor: '#ff5252' },
+                ]}
+                onPress={openDialogDeleteReplay}
+              >
+                <Text style={style.textStyle}>Delete</Text>
+                <Icon
+                  name="trash-o"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
+            <View style={style.button_con}>
+              <Pressable
+                style={[
+                  style.button,
+                  { backgroundColor: '#2A416A', borderColor: '#2A416A' },
+                ]}
+                onPress={() => {
+                  setModalVisible(false);
+                }}
+              >
+                <Text style={style.textStyle}>Close</Text>
+                <Icon
+                  name="close"
+                  style={{ marginRight: 5 }}
+                  size={18}
+                  color="white"
+                />
+              </Pressable>
+            </View>
           </View>
-        )}
-      </View>
-    </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={editModalVisible}
+      >
+        <View style={style.centeredViewEdit}>
+          <View style={style.modalViewStatusEdit}>
+            <View style={{ width: '100%', marginTop: 25 }}>
+              <View style={{ width: '100%', alignItems: 'center' }}>
+                <View style={{ backgroundColor: '#F5F4F4', width: '85%' }}>
+                  <TextInput
+                    onChangeText={handleInput}
+                    value={str}
+                    placeholder="new comment"
+                    style={{ width: '100%' }}
+                  />
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                width: '100%',
+                paddingHorizontal: 15,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 25,
+              }}
+            >
+              <Pressable
+                onPress={() => {
+                  setEditModalVisible(false);
+                }}
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: '#2A416A',
+                  paddingTop: 10,
+                  paddingBottom: 10,
+                  paddingRight: 20,
+                  paddingLeft: 20,
+                  borderRadius: 10,
+                  borderWidth: 1,
+                  borderColor: '#2A416A',
+                }}
+              >
+                <Text style={{ color: 'white', fontSize: 15, marginRight: 5 }}>
+                  Close
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={updateReplay}
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: '#009688',
+                  paddingTop: 10,
+                  paddingBottom: 10,
+                  paddingRight: 10,
+                  paddingLeft: 10,
+                  borderRadius: 10,
+                  borderWidth: 1,
+                  borderColor: '#009688',
+                }}
+              >
+                <Text style={{ color: 'white', fontSize: 15, marginRight: 5 }}>
+                  Update
+                </Text>
+                <Icon name="check" size={20} style={{ color: 'white' }} />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
 
@@ -213,5 +355,80 @@ const style = StyleSheet.create({
     marginTop: 10,
     color: 'black',
     fontSize: 12,
+  },
+
+  centeredView2: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalViewStatus: {
+    width: '100%',
+    height: 280,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    paddingTop: 20,
+    alignItems: 'flex-start',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  centeredViewEdit: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  modalViewStatusEdit: {
+    width: '100%',
+    height: 500,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    paddingTop: 20,
+    alignItems: 'flex-start',
+    marginTop:-10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  button_con: {
+    marginTop: 15,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '60%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    marginRight: 10,
   },
 });
